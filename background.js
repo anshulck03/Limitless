35c2d4c0613d0ee324f5a586aadaccd3dd9f1855
Anshul Karanam <ashkaranam@gmail.com>
2023-12-17 13:50:30 -0800

    pomodoro modifications

--- background.js
+++ background.js
@@ -1,15 +1,22 @@
 let countdown;
-let timerDuration = 1500; // 25 minutes
+let timerType = 'work'; // can be 'work', 'shortBreak', or 'longBreak'
+let cycleCount = 0;
 let endTime;
 
-function startTimer(duration) {
-  timerDuration = duration;
-  endTime = Date.now() + timerDuration * 1000;
+function startTimer(duration, type) {
+  timerType = type;
+  endTime = Date.now() + duration * 1000;
+  clearInterval(countdown);
   countdown = setInterval(() => {
     const timeLeft = Math.round((endTime - Date.now()) / 1000);
     if (timeLeft <= 0) {
       clearInterval(countdown);
       countdown = null;
+      cycleCount = timerType === 'work' ? (cycleCount + 1) % 4 : cycleCount;
+      chrome.runtime.sendMessage({ 
+        action: timerType === 'work' ? 'pomodoroComplete' : 'breakComplete',
+        cycleCount
+      });
     }
     chrome.runtime.sendMessage({ action: 'updateTimer', timeLeft });
   }, 1000);
@@ -23,15 +30,15 @@ function resetTimer() {
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'startTimer') {
-    startTimer(message.duration);
+    startTimer(message.duration, message.type);
   } else if (message.action === 'resetTimer') {
     resetTimer();
-  }
-  if (message.action === 'getTimerState') {
-    sendResponse(getTimerState());
+  } else if (message.action === 'getTimerState') {
+    sendResponse({ 
+      running: countdown !== null, 
+      timeLeft: Math.round((endTime - Date.now()) / 1000),
+      timerType,
+      cycleCount 
+    });
   }
 });
-
-function getTimerState() {
-  return { running: countdown !== null, timeLeft: Math.round((endTime - Date.now()) / 1000) };
-}
--- popup.js
+++ popup.js
@@ -1,21 +1,23 @@
 const timerDisplay = document.querySelector('#timer');
 const startButton = document.querySelector('#startButton');
+const shortBreakButton = document.querySelector('#shortBreakButton');
+const longBreakButton = document.querySelector('#longBreakButton');
 const resetButton = document.querySelector('#resetButton');
 
 startButton.addEventListener('click', () => {
-  chrome.runtime.sendMessage({ action: 'startTimer', duration: 1500 });
+  chrome.runtime.sendMessage({ action: 'startTimer', duration: 1500, type: 'work' });
+});
+
+shortBreakButton.addEventListener('click', () => {
+  chrome.runtime.sendMessage({ action: 'startTimer', duration: 300, type: 'shortBreak' });
+});
+
+longBreakButton.addEventListener('click', () => {
+  chrome.runtime.sendMessage({ action: 'startTimer', duration: 900, type: 'longBreak' });
 });
 
 resetButton.addEventListener('click', () => {
   chrome.runtime.sendMessage({ action: 'resetTimer' });
-});
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === 'updateTimer') {
-    updateDisplay(message.timeLeft);
-  } else if (message.action === 'resetTimer') {
-    updateDisplay(1500); // Reset to 25 minutes
-  }
 });
 
 function updateDisplay(seconds) {
@@ -24,7 +26,18 @@ function updateDisplay(seconds) {
   timerDisplay.textContent = `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
 }
 
-// On popup open, request current timer state
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === 'updateTimer') {
+    updateDisplay(message.timeLeft);
+  } else if (message.action === 'resetTimer') {
+    updateDisplay(1500); // 25 minutes
+  } else if (message.action === 'pomodoroComplete') {
+    alert("Time for a break!");
+  } else if (message.action === 'breakComplete') {
+    alert("Time to get back to work!");
+  }
+});
+
 document.addEventListener('DOMContentLoaded', () => {
   chrome.runtime.sendMessage({ action: 'getTimerState' });
 });
