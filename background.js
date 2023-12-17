83da2f8aa06e37db21d8c5bee43939ce325bec9f
Anshul Karanam <ashkaranam@gmail.com>
2023-12-17 14:34:04 -0800

    got rid of distraction blocker for now...

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 let countdown;
-let timerType = 'work'; // can be 'work', 'shortBreak', or 'longBreak'
+let timerType = 'work'; // Types: 'work', 'shortBreak', or 'longBreak'
 let cycleCount = 0;
 let endTime;
 
@@ -14,12 +14,8 @@ function startTimer(duration, type) {
       countdown = null;
       cycleCount = timerType === 'work' ? (cycleCount + 1) % 4 : cycleCount;
       chrome.runtime.sendMessage({ 
-        action: timerType === 'work' ? 'pomodoroComplete' : 'breakComplete',
-        cycleCount
+        action: timerType === 'work' ? 'pomodoroComplete' : 'breakComplete'
       });
-      if (timerType !== 'work') {
-        updateBlockedSites([]);
-      }
     }
     chrome.runtime.sendMessage({ action: 'updateTimer', timeLeft });
   }, 1000);
@@ -29,44 +25,12 @@ function resetTimer() {
   clearInterval(countdown);
   countdown = null;
   chrome.runtime.sendMessage({ action: 'resetTimer' });
-  updateBlockedSites([]);
-}
-
-function updateBlockedSites(sites) {
-  chrome.declarativeNetRequest.updateDynamicRules({
-    removeRuleIds: new Array(100).fill().map((_, i) => i + 1),
-    addRules: sites.map((site, index) => ({
-      id: index + 1,
-      priority: 1,
-      action: { type: 'block' },
-      condition: {
-        urlFilter: `*://*.${site}/*`,
-        resourceTypes: ['main_frame']
-      }
-    }))
-  });
 }
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'startTimer') {
     startTimer(message.duration, message.type);
-    if (message.type === 'work') {
-      chrome.storage.local.get(['blockedSites'], function(result) {
-        if (result.blockedSites) {
-          updateBlockedSites(result.blockedSites);
-        }
-      });
-    } else {
-      updateBlockedSites([]);
-    }
   } else if (message.action === 'resetTimer') {
     resetTimer();
-  } else if (message.action === 'getTimerState') {
-    sendResponse({ 
-      running: countdown !== null, 
-      timeLeft: Math.round((endTime - Date.now()) / 1000),
-      timerType,
-      cycleCount 
-    });
   }
 });
--- popup.js
+++ popup.js
@@ -3,23 +3,6 @@ const startButton = document.querySelector('#startButton');
 const shortBreakButton = document.querySelector('#shortBreakButton');
 const longBreakButton = document.querySelector('#longBreakButton');
 const resetButton = document.querySelector('#resetButton');
-const blockedSitesInput = document.querySelector('#blockedSites');
-
-// Load the blocked sites from storage when the popup is opened
-document.addEventListener('DOMContentLoaded', () => {
-  chrome.storage.local.get(['blockedSites'], function(result) {
-    if (result.blockedSites) {
-      blockedSitesInput.value = result.blockedSites.join('\n');
-    }
-    chrome.runtime.sendMessage({ action: 'getTimerState' });
-  });
-});
-
-// Save the blocked sites to storage when the user changes them
-blockedSitesInput.addEventListener('change', () => {
-  const sitesToBlock = blockedSitesInput.value.split('\n').map(s => s.trim()).filter(Boolean);
-  chrome.storage.local.set({ blockedSites: sitesToBlock });
-});
 
 function updateDisplay(seconds) {
   const minutes = Math.floor(seconds / 60);
@@ -47,10 +30,10 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'updateTimer') {
     updateDisplay(message.timeLeft);
   } else if (message.action === 'resetTimer') {
-    updateDisplay(1500); // 25 minutes
-  } else if (message.action === 'pomodoroComplete') {
-    alert("Time for a break!");
-  } else if (message.action === 'breakComplete') {
-    alert("Time to get back to work!");
+    updateDisplay(1500); // Reset to 25 minutes
   }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  chrome.runtime.sendMessage({ action: 'getTimerState' });
 });
